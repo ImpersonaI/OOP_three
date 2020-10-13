7cd6b8ec196d55d6bd3f003547423e70c6773c39
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-10-13 15:16:34 +0300

    Add files via upload

---
+++ OOP_Three/OOP_Three/DoTask.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Three
+{
+    class DoTask
+    {
+        static private int N = 0, M = 0;
+        public void Input()
+        {
+            Console.WriteLine("Input N");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input M");
+            M = Convert.ToInt32(Console.ReadLine());
+
+            double[,] array = new double[N, M];
+            double[] arrayM = new double[M]; //array for summarizing
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+
+                    Console.WriteLine("Input array[" + (i + 1).ToString() + "]" + "[" + (j + 1).ToString() + "]");
+                    array[i, j] = Convert.ToDouble(Console.ReadLine());
+
+                    arrayM[i] += array[i, j];
+                }
+                arrayM[i] /= M;
+            }
+
+            Console.WriteLine("Inputted array");
+             
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+
+                    Console.Write( array[i,j] + "\t" );
+                    
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Среднее арифметичесоке");
+
+            for (int i = 0; i < M; i++)
+            {
+
+                Console.Write(arrayM[i] + "\t");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (array[i,j] < arrayM[i])
+                    {
+                        array[i, j] = 0;
+                    }
+
+                    if (array[i, j] > arrayM[i])
+                    {
+                        array[i, j] = 1;
+                    }
+
+
+                    Console.Write(array[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public void DoMath()
+        {
+
+
+        }
+    }
+}
---
+++ OOP_Three/OOP_Three/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoTask dotask = new DoTask();
+
+            dotask.Input();
+
+            Console.ReadKey();
+        }
+    }
+}
